de12cd22ad0f4de25cf5bddc881f9d373398df39
Amogh Divekar <amoghdivekar22@gmail.com>
2022-01-01 19:43:23 +0530

    initial commit
    App done

---
+++ app.js
@@ -0,0 +1,36 @@
+const initialPrice = document.querySelector("#initial-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener('click', submithandler);
+
+function submithandler() {
+    var ip = initialPrice.value;
+    var qty = stocksQuantity.value;
+    var curr = currentPrice.value;
+
+    calculateProfitAndLoss(ip, qty, curr); 
+}
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if(initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+
+        showOutput(`Hey the loss is ${loss} and the loss percent is ${lossPercentage}%`);
+    } else if(current > initial) {
+        var profit = (current - initial) * 100; 
+        var profitPercentage = (profit / initial) * 100;
+
+        showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}%`);
+    } else { 
+        showOutput('No Pain No Gain and No Gain No Pain!');
+    }
+}
+
+function showOutput(message) {
+    outputBox.innerHTML = message;
+    outputBox.style.color = 'green';
+}
